11ea7825f096f2f49758972c1b6dcc79517704fe
Mohammed Aftab Siddique <aftabsd786@gmail.com>
2026-08-08 22:09:21 +0530

    build: working solution

--- dt_sms_plugin/__main__.py
+++ dt_sms_plugin/__main__.py
@@ -17,36 +17,21 @@ class ExtensionImpl(Extension):
     def initialize(self) -> None:
         try:
             log_info(self.logger, "Initializing DT SMS Plugin...")
-
-            print("CONFIG TYPE:", type(self.activation_config.config))
-            print("CONFIG KEYS:", list(self.activation_config.config.keys()))
-
-            print("STEP 1")
+            
             self.settings = load_settings(self.activation_config)
 
-            print("STEP 2")
             validate_settings(self.settings)
 
-            print("STEP 3")
             self.metrics = MetricsPublisher(self)
 
-            print("STEP 4")
             self.cache = CacheManager(Path())
             self.cache.load()
 
-            print("STEP 5")
-            config = self.activation_config.config
-
-            print("CONFIG:")
-            print(config)
-
-            print("STEP 6")
             self.dt_client = DynatraceClient(
                 tenant_url=self.settings.dynatrace.url,
                 api_token=self.settings.dynatrace.api_token,
             )
 
-            print("STEP 7")
             self.sms_client = SmsClient(
                 logger=self.logger,
                 url=self.settings.sms_api.url,
@@ -56,14 +41,12 @@ class ExtensionImpl(Extension):
                 dry_run=self.settings.dry_run,
             )
 
-            print("STEP 8")
             self.engine = SmsEngine(
                 settings=self.settings,
                 cache=self.cache,
                 sms_client=self.sms_client,
             )
 
-            print("STEP 9")
             log_info(self.logger, "Initialization completed.")
 
         except Exception:
@@ -76,8 +59,8 @@ class ExtensionImpl(Extension):
         try:
             log_info(self.logger, "Query started.")
 
-            # if self.dt_client is None:
-            #     return
+            if self.dt_client is None:
+                return
 
             problems = self.dt_client.fetch_problems(
                 lookback_minutes=self.settings.lookback_window,
